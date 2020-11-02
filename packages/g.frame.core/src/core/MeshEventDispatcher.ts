05a6fd6b9f4bfadc1a2334c6c14e4b829811fe0b
Oleksii Maksymov <oleksii.maksymov@verybigthings.com>
2020-11-02 22:22:43 +0200

    Fixed MeshEventDescriptor

--- packages/g.frame.core/src/core/MeshEventDispatcher.ts
+++ packages/g.frame.core/src/core/MeshEventDispatcher.ts
@@ -2,6 +2,9 @@ import {Object3D} from 'three';
 import {ObjectEventDescriptor, ObjectEventDispatcher} from './ObjectEventDispatcher';
 
 export class MeshEventDescriptor extends ObjectEventDescriptor<Object3D> {
+    constructor(public eventName: string, public mesh: Object3D, public callback: Function, public condition?: Function) {
+        super(eventName, mesh, callback, condition);
+    }
 }
 
 export class MeshEventDispatcher extends ObjectEventDispatcher<Object3D> {
